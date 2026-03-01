cd0e598def32cd6b190fceac3158a78e11a2327f
Karunyah-Nimallan <karunyah@tamu.edu>
2026-03-01 16:11:19 -0600

    Creating Fake Datasets for the vault

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,25 @@
 #uvicorn backend.app.main:app --reload //to run the code
+import string
+
 from fastapi import FastAPI
 app = FastAPI() #Creates a FastAPI instance
-@app.get("/")
-def root():
-    return {"message": "Hello World!"} #Returns a JSON response with the message "Hello World!"
+#frontend_input = input("This is what the frontend will send to the backend: ") #Simulates receiving input from the frontend
+frontend_input = "Example Vault" #Simulates receiving input from the frontend
+
+mock_vault = [
+    {"id": 1, "name": "Example Vault", "username": "example_user", "email": "example@example.com", "password": "example_password", "website": "example.com"}, 
+    {"id": 2, "name": "Another Vault", "username": "another_user", "email": "another@another.com", "password": "another_password", "website": "another.com"}, 
+    {"id": 3, "name": "Third Vault", "username": "third_user", "email": "third@third.com", "password": "third_password", "website": "third.com"}
+]
+
+@app.get("/{frontend_input}")
+def root(frontend_input: str = frontend_input):
+    for i in range(len(mock_vault)):
+        if mock_vault[i]["name"] == frontend_input:
+            return mock_vault[i]
+    return {"message": "Frontend input not found in vault."}
+
+
+
+    #Returns a JSON response with the message "Hello World!"
+
