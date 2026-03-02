d7b120ae75872cda4e2352703fcc2740895d5c37
Karunyah-Nimallan <karunyah@tamu.edu>
2026-03-02 10:24:19 -0600

    Bye Bye Yellow Squiggly Line

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+
+app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
+
+#fastapi dev backend/api/auth.py
