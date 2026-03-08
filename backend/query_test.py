261abf8270d30b52ac011be73428722cf506b563
Abby Saldana <abbysaldana@tamu.edu>
2026-03-08 15:29:50 -0500

    Updated query_test.py

--- backend/query_test.py
+++ backend/query_test.py
@@ -1,20 +1,16 @@
 from app.database import Base, engine, get_session
 from models import User, VaultEntry
-from query_helper_functions import add_user, add_vault_entry
+from query_helper_functions import *
 
-#make tables
+#creates sqllite file "vault.db"
 Base.metadata.create_all(bind=engine)
 
-#open a sesssion = get_session()
-session = get_session()
+#create session
+with get_session() as session:
+    bob = add_user(session, email="bob@example.com", username="bob", password="hashedpassword")
+    add_vault_entry(session, owner_id=bob.id, account="netfilx", password="secret123")
 
-bob = add_user(session, email="bob@example.com", username="bob", password="hashedpassword")
-add_vault_entry(session, owner_id=bob.id, account="netfilx", password="secret123")
+    molly = add_user(session, email="molly@example.com", username="molly00", password="hashedpassword")
+    add_vault_entry(session, owner_id=molly.id, account="gmail", password="secret123")
+    add_vault_entry(session, owner_id=molly.id, account="spotify", password="password")
 
-molly = add_user(session, email="molly@example.com", username="molly00", password="hashedpassword")
-add_vault_entry(session, owner_id=molly.id, account="gmail", password="secret123")
-add_vault_entry(session, owner_id=molly.id, account="spotify", password="password")
-
-
-#close session
-session.close()
