e4420b004f8b04b28b4ffe42d11d2302f5fe0ba3
Karunyah-Nimallan <karunyah@tamu.edu>
2026-03-29 15:39:14 -0500

    FInished API

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from typing import Optional
 from pydantic import BaseModel
+from backend.app.database import get_db
 
 # Example (Basic Testing)
 # router = APIRouter()
@@ -39,17 +40,39 @@ def root():
     
 @router.post("/vault")
 def create_vault_entry(entry: VaultEntry):
-    return {
-        #Eventually, we will store the entry in a sql database
-        "message": "Vault entry parsed successfully",
-        "title": entry.title,
-        "user_id": entry.user_id,
-    }
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        INSERT INTO vault_entries
+        (user_id, title, encrypted_data, iv, salt, created_at, updated_at)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            entry.user_id,
+            entry.title,
+            entry.encrypted_data,
+            entry.iv,
+            entry.salt,
+            entry.created_at,
+            entry.updated_at
+        )
+    )
+    conn.commit()
+    conn.close()
+    return {"message": "Vault entry created"}
+    
     
 @router.get("/vault/{entry_id}")
 def get_vault_entry(entry_id: int):
-    return {
-        #Eventually, it will search data base for this entry and then return it.
-        "message": "Vault entry retrieved successfully",
-        "user_id": entry_id,
-    }
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM vault_entries WHERE id = ?",
+        (entry_id,)
+    )
+
+    entry = cursor.fetchone()
+    conn.close()
+    return dict(entry)
